45352fb2ae66358d60cfbd7127d394e35d723dad
krupal purohit <krupalpurohit1997@gmail.com>
2018-05-13 12:01:05 +0530

    added app doc

--- coffeehouse/urls.py
+++ coffeehouse/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
@@ -23,4 +24,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',TemplateView.as_view(template_name='homepage.html')),
     url(r'^about/',about_views.contact),
+    url(r'^admin/doc/',include('django.contrib.admindocs.urls')),
 ]
